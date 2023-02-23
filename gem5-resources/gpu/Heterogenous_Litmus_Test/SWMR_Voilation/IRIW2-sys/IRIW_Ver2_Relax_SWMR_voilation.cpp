c13182c9ee43e1858c21adab3c0f65163f6ef05b
sukarnagarwal <68431807+sukarnagarwal@users.noreply.github.com>
2023-02-23 12:30:48 +0000

    Update IRIW_Ver2_Relax_SWMR_voilation.cpp

--- gem5-resources/gpu/Heterogenous_Litmus_Test/SWMR_Voilation/IRIW2-sys/IRIW_Ver2_Relax_SWMR_voilation.cpp
+++ gem5-resources/gpu/Heterogenous_Litmus_Test/SWMR_Voilation/IRIW2-sys/IRIW_Ver2_Relax_SWMR_voilation.cpp
@@ -1,3 +1,32 @@
+/*
+    m5threads, a pthread library for the M5 simulator
+    Copyright (C) 2009, Stanford University
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+    
+    
+    Litmus Test Developed by 
+
+Sukarn Agarwal (Email: sagarwa2@ed.ac.uk)
+Nicolai Oswald (Email: nicolai.oswald@ed.ac.uk)    
+    
+*/
+
+
+
+
 #include <assert.h>
 #include <pthread.h>
 #include <stdio.h>
