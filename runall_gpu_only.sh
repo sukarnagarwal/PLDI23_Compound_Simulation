7a0f265e5da99a560db6c29602ac58f2046c42db
sukarnagarwal <68431807+sukarnagarwal@users.noreply.github.com>
2023-02-28 17:09:40 +0000

    Add files via upload

---
+++ runall_gpu_only.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+function genStatsName()
+{
+	statsName="stats_"
+	statsName+=$k
+	statsName+="_GPU_only_2048runs"
+}
+
+function genFileName()
+{
+	if [[ $j == "-sys" ]]; then
+		fileName=$i
+		fileName+="_Relax"
+	else 
+		fileName=$i
+		fileName+="_RelAcq_WO_Atomic_Fence"
+	fi
+}
+
+
+for i in MP LB SB IRIW
+do
+	for j in -sys -sys-F
+	do 
+		k=$i
+		k+=$j
+		if [[ $k == "LB-sys-F" ]]; then
+			continue
+		fi
+		echo "Executing " $k
+		genStatsName
+		echo "statsName: " $statsName
+		genFileName
+		echo "fileName: " $fileName
+	sudo docker run -u $UID:$GID --volume $(pwd):$(pwd) -w $(pwd) gcn-gpu2 gem5/build/GCN3_X86/gem5.opt --stats-file=$statsName gem5/configs/example/apu_se.py --cpu-type=DerivO3CPU -n 3 -c gem5-resources/gpu/litmusTest/$i/$k/bin/$fileName
+	done
+done
+
+
+
