f8852cb57c7e0ad69c026339b2ee39b540cffd92
sukarnagarwal <68431807+sukarnagarwal@users.noreply.github.com>
2023-03-02 11:58:23 +0000

    Add files via upload

---
+++ gem5-resources/gpu/Heterogenous_Litmus_Test/SWMR_Voilation/cleanall_SWMR_voilation.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+extra=""
+if [ ! -z "$1" ]
+then 
+	extra="$1"
+fi
+
+for i in MP LB SB IRIW
+do
+	cd $i
+	if [[ $i == "MP" || $i == "IRIW" ]]; then 
+		array=( 1 2 )
+	else
+		array=( 1 ) 
+	fi
+	for j in "${array[@]}" 
+	do
+		for k in -sys -sys-F 	
+		do
+			if [[ $i == "MP" || $i == "IRIW" ]]; then
+				l=$i
+				l+=$j
+				l+=$k
+				if [[ $l == "IRIW1-sys-F" ]]; then
+					continue
+				fi
+				cd $l
+				echo "Building " $l 
+				sudo docker run --rm -v ${PWD}:${PWD} -w ${PWD} -u $UID:$GID gcn-gpu2 make clean
+				cd ..
+			else
+				l=$i
+				l+=$k
+				if [[ $l == "LB-sys-F" ]]; then
+					continue
+				fi
+				cd $l
+				echo "Building " $l
+				sudo docker run --rm -v ${PWD}:${PWD} -w ${PWD} -u $UID:$GID gcn-gpu2 make clean
+				cd ..
+			fi	
+		done	
+			
+	done		
+	cd ..		
+done
+
