3fd962042bf205444a0b2fcf42d97e07c87accf8
sukarnagarwal <68431807+sukarnagarwal@users.noreply.github.com>
2023-03-03 17:07:46 +0000

    Update MP_RelAcq_WO_Atomic_Fence.cpp

--- gem5-resources/gpu/GPU_Litmus_test/MP/MP-sys-F/MP_RelAcq_WO_Atomic_Fence.cpp
+++ gem5-resources/gpu/GPU_Litmus_test/MP/MP-sys-F/MP_RelAcq_WO_Atomic_Fence.cpp
@@ -161,7 +161,7 @@ int check_output(struct r_gpu0* v_cpu0, struct r_gpu1* v_gpu0, unsigned t_range)
         cout << "Num Valid test: " << (t_range - res_cpu0_gpu1) << endl;
         cout << "Num Invalid test: " << res_cpu0_gpu1 << endl;
         std::cout << "=========================================================================" << std::endl;
-        std::cout << "	\t GPU-Only MP-sys Allowed  \t " << std::endl;						
+        std::cout << "	\t GPU-Only MP-sys-F Allowed  \t " << std::endl;						
         std::cout << "=========================================================================" << std::endl;
         return 2;
     }
