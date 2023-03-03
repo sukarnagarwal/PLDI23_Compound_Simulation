4d7c46d4cb0fc65fe20a6f2e26d81c0fbf3da4fb
sukarnagarwal <68431807+sukarnagarwal@users.noreply.github.com>
2023-03-03 17:26:28 +0000

    Update LB_Relax.cpp

--- gem5-resources/gpu/GPU_Litmus_test/LB/LB-sys/LB_Relax.cpp
+++ gem5-resources/gpu/GPU_Litmus_test/LB/LB-sys/LB_Relax.cpp
@@ -149,6 +149,9 @@ int check_output(struct r_gpu0* v_cpu0, struct r_gpu1* v_gpu0, unsigned t_range)
         cout << "Count (a:2 and b:2): " << res_cpu2_gpu2 << endl;
         cout << "Num Valid test: " << (t_range - res_cpu1_gpu1) << endl;
         cout << "Num Invalid test: " << res_cpu1_gpu1 << endl;
+        std::cout << "=========================================================================" << std::endl;
+        std::cout << "	\t GPU-Only LB-sys Disallowed  \t " << std::endl;						
+        std::cout << "=========================================================================" << std::endl; 
         return 0;
     } else {
         cout << "Fail!" << endl;
@@ -159,6 +162,9 @@ int check_output(struct r_gpu0* v_cpu0, struct r_gpu1* v_gpu0, unsigned t_range)
         cout << "Count (a:2 and b:2): " << res_cpu2_gpu2 << endl;
         cout << "Num Valid test: " << (t_range - res_cpu1_gpu1) << endl;
         cout << "Num Invalid test: " << res_cpu1_gpu1 << endl;
+        std::cout << "=========================================================================" << std::endl;
+        std::cout << "	\t GPU-Only LB-sys Allowed  \t " << std::endl;						
+        std::cout << "=========================================================================" << std::endl; 
         return 2;
     }
 
