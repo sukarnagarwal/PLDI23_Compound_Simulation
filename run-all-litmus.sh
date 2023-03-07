4976a5fe126de23e8550a3501510b986a29462e9
Andres Goens <andres.goens@ed.ac.uk>
2023-03-07 13:39:59 +0000

    Make all-litmus script generate csv files.

--- run-all-litmus.sh
+++ run-all-litmus.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-BASEDIR=`cwd`
-OUTPUT=results.log
 
-echo "Running CPU only" | tee -a $OUTPUT
-./runall_cpu_only.sh 2>> build.log | tee -a $OUTPUT
+LOG=litmus.log
+OUTPUT=results.csv
 
-echo "Running GPU only" | tee -a $OUTPUT
-./runall_gpu_only.sh 2>> build.log | tee -a $OUTPUT
+./runall_cpu_only.sh 2>> build.log | tee $LOG
+./runall_gpu_only.sh 2>> build.log | tee -a $LOG
+./runall_CTA_scope.sh 2>> build.log | tee -a $LOG
+./runall_Hetero_only.sh 2>> build.log | tee -a $LOG
+./runall_SWMR_Voilation.sh 2>> build.log | tee -a $LOG
 
-echo "Running CTA scope" | tee -a $OUTPUT
-./runall_CTA_scope.sh 2>> build.log | tee -a $OUTPUT
-
-echo "Running heterogeneous only" | tee -a $OUTPUT
-./runall_Hetero_only.sh 2>> build.log | tee -a $OUTPUT
-
-echo "Running non-SWMR tests" | tee -a $OUTPUT
-./runall_SWMR_Voilation.sh 2>> build.log | tee -a $OUTPUT
+echo "Type,Litmus,Result" > $OUTPUT
+sed -n -e '/llowed/p' $LOG | sed 's/^[[:space:]]*//' | tr ' ' ',' >> $OUTPUT
