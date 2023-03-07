a32a6ab1a877e77fa999730e72f3312099a45a6d
Andres Goens <andres.goens@ed.ac.uk>
2023-03-07 12:38:36 +0000

    redirect errors

--- run-all-litmus.sh
+++ run-all-litmus.sh
@@ -3,16 +3,16 @@ BASEDIR=`cwd`
 OUTPUT=results.log
 
 echo "Running CPU only" | tee -a $OUTPUT
-./runall_cpu_only.sh | tee -a $OUTPUT
+./runall_cpu_only.sh 2>> build.log | tee -a $OUTPUT
 
 echo "Running GPU only" | tee -a $OUTPUT
-./runall_gpu_only.sh | tee -a $OUTPUT
+./runall_gpu_only.sh 2>> build.log | tee -a $OUTPUT
 
 echo "Running CTA scope" | tee -a $OUTPUT
-./runall_CTA_scope.sh | tee -a $OUTPUT
+./runall_CTA_scope.sh 2>> build.log | tee -a $OUTPUT
 
 echo "Running heterogeneous only" | tee -a $OUTPUT
-./runall_Hetero_only.sh | tee -a $OUTPUT
+./runall_Hetero_only.sh 2>> build.log | tee -a $OUTPUT
 
 echo "Running non-SWMR tests" | tee -a $OUTPUT
-./runall_SWMR_Voilation.sh | tee -a $OUTPUT
+./runall_SWMR_Voilation.sh 2>> build.log | tee -a $OUTPUT
